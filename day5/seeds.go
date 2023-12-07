06e90bad124af9cb15a89b6d3f4747792566a2bc
rgvanwesep <rvanwes@gmail.com>
2023-12-07 02:34:08 +0000

    Attempt at part 2

--- day5/seeds.go
+++ day5/seeds.go
@@ -16,6 +16,104 @@ const (
 	humidityToLocationHeader    = "humidity-to-location map:"
 )
 
+type Range struct {
+	Start  int
+	Length int
+}
+
+func (r Range) Intersect(other Range) Range {
+	start := r.Start
+	if other.Start > start {
+		start = other.Start
+	}
+	end := r.Start + r.Length
+	if other.Start+other.Length < end {
+		end = other.Start + other.Length
+	}
+	if end < start {
+		return Range{start, 0}
+	}
+	return Range{start, end - start}
+}
+
+func (r Range) Subtract(other Range) []Range {
+	intersect := r.Intersect(other)
+	if intersect.Length == 0 {
+		return []Range{r}
+	}
+	ranges := make([]Range, 0)
+	if intersect.Start > r.Start {
+		rtmp := Range{r.Start, intersect.Start - r.Start}
+		if rtmp.Length > 0 {
+			ranges = append(ranges, rtmp)
+		}
+	}
+	if intersect.Start+intersect.Length < r.Start+r.Length {
+		rtmp := Range{intersect.Start + intersect.Length, r.Start + r.Length - intersect.Start - intersect.Length}
+		if rtmp.Length > 0 {
+			ranges = append(ranges, rtmp)
+		}
+	}
+	return ranges
+}
+
+func (r Range) Union(other Range) []Range {
+	if r.Length == 0 {
+		return []Range{other}
+	}
+	if other.Length == 0 {
+		return []Range{r}
+	}
+	intersect := r.Intersect(other)
+	if intersect.Length == 0 {
+		return []Range{r, other}
+	}
+	start := r.Start
+	if other.Start < start {
+		start = other.Start
+	}
+	end := r.Start + r.Length
+	if other.Start+other.Length > end {
+		end = other.Start + other.Length
+	}
+	return []Range{{start, end - start}}
+}
+
+type RangeSet []Range
+
+func NewRangeSet(ranges []Range) RangeSet {
+	rangeSet := make(RangeSet, 0)
+	for i := 0; i < len(ranges); i++ {
+		for j := i + 1; j < len(ranges); j++ {
+			ranges = append(ranges, ranges[i].Union(ranges[j])...)
+		}
+	}
+	return rangeSet
+}
+
+func (r RangeSet) Intersect(other RangeSet) RangeSet {
+	ranges := make(RangeSet, 0)
+	for _, r1 := range r {
+		for _, r2 := range other {
+			intersection := r1.Intersect(r2)
+			if intersection.Length > 0 {
+				ranges = append(ranges, intersection)
+			}
+		}
+	}
+	return ranges
+}
+
+func (r RangeSet) Subtract(other RangeSet) RangeSet {
+	ranges := make(RangeSet, 0)
+	for _, r1 := range r {
+		for _, r2 := range other {
+			ranges = append(ranges, r1.Subtract(r2)...)
+		}
+	}
+	return ranges
+}
+
 type Seeds []int
 
 func NewSeeds(input string) Seeds {
@@ -42,6 +140,13 @@ func (r RangeMapItem) Apply(input int) (int, bool) {
 	return input - r.SrcStart + r.DstStart, true
 }
 
+type RangeMap []RangeMapItem
+
+func (r RangeMap) Apply(RangeSet) RangeSet {
+	ranges := make(RangeSet, 0)
+	return ranges
+}
+
 func NewRangeMapItem(input string) RangeMapItem {
 	input = strings.TrimRight(input, "\n")
 	split := strings.Split(input, " ")
@@ -51,12 +156,12 @@ func NewRangeMapItem(input string) RangeMapItem {
 	return RangeMapItem{dstStart, srcStart, length}
 }
 
-type RangeMap struct {
+type NamedRangeMap struct {
 	Header string
 	Items  []RangeMapItem
 }
 
-func NewRangeMap(inputs []string) RangeMap {
+func NewNamedRangeMap(inputs []string) NamedRangeMap {
 	header := strings.TrimRight(inputs[0], "\n")
 	items := make([]RangeMapItem, 0)
 	for _, input := range inputs[1:] {
@@ -65,10 +170,10 @@ func NewRangeMap(inputs []string) RangeMap {
 		}
 		items = append(items, NewRangeMapItem(input))
 	}
-	return RangeMap{header, items}
+	return NamedRangeMap{header, items}
 }
 
-func (r RangeMap) Apply(input int) []int {
+func (r NamedRangeMap) Apply(input int) []int {
 	outputs := make([]int, 0)
 	for _, item := range r.Items {
 		if output, ok := item.Apply(input); ok {
@@ -83,41 +188,41 @@ func (r RangeMap) Apply(input int) []int {
 
 type Atlas struct {
 	Seeds Seeds
-	Maps  []RangeMap
+	Maps  []NamedRangeMap
 }
 
 func NewAtlas(inputs []string) Atlas {
 	atlas := Atlas{
 		Seeds: NewSeeds(inputs[0]),
-		Maps:  make([]RangeMap, 0),
+		Maps:  make([]NamedRangeMap, 0),
 	}
 	for i := 1; i < len(inputs); {
 		if strings.HasPrefix(inputs[i], seedToSoilHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else if strings.HasPrefix(inputs[i], soilToFertilizerHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else if strings.HasPrefix(inputs[i], fertilizerToWaterHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else if strings.HasPrefix(inputs[i], waterToLightHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else if strings.HasPrefix(inputs[i], lightToTemperatureHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else if strings.HasPrefix(inputs[i], temperatureToHumidityHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else if strings.HasPrefix(inputs[i], humidityToLocationHeader) {
-			rangeMap := NewRangeMap(inputs[i:])
+			rangeMap := NewNamedRangeMap(inputs[i:])
 			atlas.Maps = append(atlas.Maps, rangeMap)
 			i += len(rangeMap.Items)
 		} else {
