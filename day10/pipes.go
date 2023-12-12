97771e405b8dc514efa725828e9d90a174658312
rgvanwesep <rvanwes@gmail.com>
2023-12-12 00:58:45 +0000

    Started implementing part 2

--- day10/pipes.go
+++ day10/pipes.go
@@ -16,10 +16,17 @@ const (
 	start      = 'S'
 )
 
+const (
+	mainLoop = iota
+	leftShoulder
+	rightShoulder
+)
+
 type Node struct {
 	Value      byte
 	Coordinate Coordinate
 	Neighbors  []*Node
+	Class 	int
 }
 
 func (n *Node) Next(in *Node) (*Node, bool) {
@@ -32,6 +39,61 @@ func (n *Node) Next(in *Node) (*Node, bool) {
 	return n.Neighbors[0], true
 }
 
+func (n *Node) Shoulders(in Coordinate) [2]Coordinate {
+	var shoulders [2]Coordinate
+	switch n.Value {
+	case vertical:
+		if in.Y > n.Coordinate.Y {
+			shoulders[0] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y}
+			shoulders[1] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y}
+		} else {
+			shoulders[0] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y}
+			shoulders[1] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y}
+		}
+	case horizontal:
+		if in.X < n.Coordinate.X {
+			shoulders[0] = Coordinate{n.Coordinate.X, n.Coordinate.Y - 1}
+			shoulders[1] = Coordinate{n.Coordinate.X, n.Coordinate.Y + 1}
+		} else {
+			shoulders[0] = Coordinate{n.Coordinate.X, n.Coordinate.Y + 1}
+			shoulders[1] = Coordinate{n.Coordinate.X, n.Coordinate.Y - 1}
+		}
+	case northEast:
+		if in.Y < n.Coordinate.Y {
+			shoulders[0] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y - 1}
+			shoulders[1] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y + 1}
+		} else {
+			shoulders[0] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y + 1}
+			shoulders[1] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y - 1}
+		}
+	case northWest:
+		if in.Y < n.Coordinate.Y {
+			shoulders[0] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y + 1}
+			shoulders[1] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y - 1}
+		} else {
+			shoulders[0] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y - 1}
+			shoulders[1] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y + 1}
+		}
+	case southWest:
+		if in.Y > n.Coordinate.Y {
+			shoulders[0] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y + 1}
+			shoulders[1] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y - 1}
+		} else {
+			shoulders[0] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y - 1}
+			shoulders[1] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y + 1}
+		}
+	case southEast:
+		if in.Y > n.Coordinate.Y {
+			shoulders[0] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y - 1}
+			shoulders[1] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y + 1}
+		} else {
+			shoulders[0] = Coordinate{n.Coordinate.X + 1, n.Coordinate.Y + 1}
+			shoulders[1] = Coordinate{n.Coordinate.X - 1, n.Coordinate.Y - 1}
+		}
+	}
+	return shoulders
+}
+
 type Coordinate struct {
 	X int
 	Y int
@@ -41,6 +103,9 @@ type Graph struct {
 	Nodes       map[Coordinate]*Node
 	Start       Coordinate
 	Distances   map[Coordinate]int
+	MainLoop    map[Coordinate]*Node
+	Outsides	map[Coordinate]bool
+	Shoulders   map[Coordinate][2]Coordinate
 	MaxDistance int
 }
 
@@ -51,10 +116,8 @@ func NewGraph(lines []string) *Graph {
 	for y, line := range lines {
 		line = strings.TrimRight(line, "\n")
 		for x, char := range line {
-			if char == ground {
-				continue
-			}
 			coord := Coordinate{x, y}
+			g.Outsides[coord] = true
 			g.Nodes[coord] = &Node{
 				Value:      byte(char),
 				Coordinate: coord,
@@ -67,7 +130,8 @@ func NewGraph(lines []string) *Graph {
 	return g
 }
 
-func (g *Graph) Connect() {
+func (g *Graph) ConnectPipes() {
+	var northAppended, southAppended, eastAppended, westAppended bool
 	for coord, node := range g.Nodes {
 		north, northOK := g.Nodes[Coordinate{coord.X, coord.Y - 1}]
 		south, southOK := g.Nodes[Coordinate{coord.X, coord.Y + 1}]
@@ -89,31 +153,51 @@ func (g *Graph) Connect() {
 		case start:
 			if northOK && (north.Value == vertical || north.Value == southEast || north.Value == southWest) {
 				node.Neighbors = append(node.Neighbors, north)
+				northAppended = true
 			}
 			if southOK && (south.Value == vertical || south.Value == northEast || south.Value == northWest) {
 				node.Neighbors = append(node.Neighbors, south)
+				southAppended = true
 			}
 			if eastOK && (east.Value == horizontal || east.Value == northWest || east.Value == southWest) {
 				node.Neighbors = append(node.Neighbors, east)
+				eastAppended = true
 			}
 			if westOK && (west.Value == horizontal || west.Value == northEast || west.Value == southEast) {
 				node.Neighbors = append(node.Neighbors, west)
+				westAppended = true
+			}
+			switch {
+			case northAppended && southAppended:
+				node.Value = vertical
+			case eastAppended && westAppended:
+				node.Value = horizontal
+			case northAppended && eastAppended:
+				node.Value = northEast
+			case northAppended && westAppended:
+				node.Value = northWest
+			case southAppended && eastAppended:
+				node.Value = southEast
+			case southAppended && westAppended:
+				node.Value = southWest
 			}
 		}
 	}
 }
 
-func (g *Graph) UpdateDistances() {
+func (g *Graph) MapMainLoop() {
 	var nextOk bool
 	g.Distances = make(map[Coordinate]int)
 	g.Distances[g.Start] = 0
 	startNode := g.Nodes[g.Start]
+	g.MainLoop = map[Coordinate]*Node{g.Start: startNode}
 	nodes := [2]*Node{startNode.Neighbors[0], startNode.Neighbors[1]}
 	prevNodes := [2]*Node{startNode, startNode}
 outer:
 	for distance := 1; nodes[0] != startNode && nodes[1] != startNode; distance++ {
 		for i, node := range nodes {
 			coord := Coordinate{node.Coordinate.X, node.Coordinate.Y}
+			g.MainLoop[coord] = node
 			if _, ok := g.Distances[coord]; !ok {
 				g.Distances[coord] = distance
 			} else {
@@ -127,11 +211,49 @@ outer:
 			prevNodes[i] = node
 		}
 	}
+
+	prev := startNode
+	node := startNode.Neighbors[0]
+	var next *Node
+	for node != startNode {
+		node.Class = mainLoop
+		g.Outsides[node.Coordinate] = false
+		g.Shoulders[node.Coordinate] = node.Shoulders(prev.Coordinate)
+		next, nextOk = node.Next(prev)
+		if !nextOk {
+			log.Fatalf("Node %v has no next", node)
+		}
+		prev, node = node, next
+	}
+	g.Outsides[g.Start] = false
+	g.Shoulders[g.Start] = startNode.Shoulders(prev.Coordinate)
+
+}
+
+func (g *Graph) ConnectOutsides() {
+	for coord, outside := range g.Outsides {
+		if !outside {
+			continue
+		}
+		node := g.Nodes[coord]
+		node.Neighbors = make([]*Node, 0)
+		for _, x := range []int{-1, 0, 1} {
+			for _, y := range []int{-1, 0, 1} {
+				if x == 0 && y == 0 {
+					continue
+				}
+				neighbor, ok := g.Nodes[Coordinate{coord.X + x, coord.Y + y}]
+				if ok && g.Outsides[neighbor.Coordinate] {
+					node.Neighbors = append(node.Neighbors, neighbor)
+				}
+			}
+		}
+	}
 }
 
 func FindFarthest(lines []string) int {
 	g := NewGraph(lines)
-	g.Connect()
-	g.UpdateDistances()
+	g.ConnectPipes()
+	g.MapMainLoop()
 	return g.MaxDistance
 }
